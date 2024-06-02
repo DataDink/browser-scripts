7b1148e897d10bcf1e83de1c59862649aebdf226
Mark Greenwald <markerdink@gmail.com>
2024-06-02 13:18:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 (() => {
   const source = 'https://datadink.github.io/browser-scripts/';
+  const queue = Promise.resolve();
   if ([...document.querySelectorAll(`script[src^="${source}"]`)].length > 1) { return; }
   for (var name of [
     'video-saver'
   ]) {
-    var script = document.createElement('script');
-    script.setAttribute('src', `${source}/${name}.js`);
-    script.setAttribute('type', 'module');
-    document.body.appendChild(script);
+    queue = queue.then(() => fetch(`${source}/${name}.js`))
   }
 })();
