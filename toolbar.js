0f78e8fdd94f93a859eedfa30b26b0076e4a2b83
Mark Greenwald <markerdink@gmail.com>
2024-06-02 13:33:42 -0700

    Update toolbar.js

--- toolbar.js
+++ toolbar.js
@@ -3,7 +3,7 @@
   get: () => {
     if (this[accessor]) { return this[accessor]; }
     var element = this;
-    var toolbar = element[accessor] = document.createElement('div');
+    var toolbar = document.createElement('div');
     toolbar.style.width = 'auto';
     toolbar.style.height = '16px';
     toolbar.style.display = 'flex';
@@ -22,6 +22,6 @@
       if (!element.parentNode) { return toolbar.parentNode?.removeChild(toolbar); }
       element.parentNode.insertBefore(toolbar, element);
     }).observe(element, { });
-    return toolbar;
+    return element[accessor] = toolbar;
   }
 }))(Symbol('toolbar'));
