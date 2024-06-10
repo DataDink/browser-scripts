45373f13a2f933ef6caeaaf54a010759263f71ee
Mark Greenwald <markerdink@gmail.com>
2024-06-10 09:40:08 -0700

    Update toolbar.js

--- toolbar.js
+++ toolbar.js
@@ -1,28 +1,36 @@
 (accessor => Object.defineProperty(HTMLElement.prototype, 'toolbar', {
   configurable: false, enumerable: true,
-  get: () => {
+  get: function() {
     if (this[accessor]) { return this[accessor]; }
     const size = '16px';
-    var element = this;
-    var toolbar = document.createElement('div');
+    const element = this;
+    const toolbar = document.createElement('div');
     toolbar.style.width = 'auto';
     toolbar.style.height = size;
     toolbar.style.display = 'flex';
-    toolbar.style.position = 'absolutea';
+    toolbar.style.position = 'absolute';
     toolbar.style.flexDirection = 'row';
     toolbar.style.justifyContent = 'flex-start';
     toolbar.style.alignItems = 'flex-start';
     toolbar.style.overflow = 'auto';
     toolbar.style.overflowY = 'hidden';
     toolbar.style.zIndex = Number.MAX_SAFE_INTEGER;
+    toolbar.addEventListener('mousedown', e => { e.stopPropagation(); e.preventDefault(); });
+    toolbar.addEventListener('mouseup', e => { e.stopPropagation(); e.preventDefault(); });
+    toolbar.addEventListener('click', e => { e.stopPropagation(); e.preventDefault(); });
     toolbar.add = function(uri, callback) {
-      var button = document.createElement('div');
+      const button = document.createElement('div');
       button.style.width = size;
       button.style.height = size;
       button.style.backgroundImage = `url("${JSON.stringify(uri)}")`;
       button.style.backgroundPosition = 'center';
       button.style.backgroundSize = 'contain';
-      button.addEventListener('click', callback.call(element));
+      button.style.border = '1px solid white';
+      button.addEventListener('click', e => {
+        e.stopPropagation();
+        e.preventDefault();
+        callback.call(element)
+      });
       toolbar.appendChild(button);
     }
     element.parentNode.insertBefore(toolbar, element);
